2ae089d52bec490851635b5ff25248103e479a17
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-06-23 10:04:29 +0300

    objektai

--- script.js
+++ script.js
@@ -140,46 +140,94 @@ aritmetika.rezultatas(); // isvedimas objekto viduje
 // console.log(aritmetika.dalyba());
 // console.log(aritmetika);
 
-document.getElementById("Patvirtinti").addEventListener("click", function() {
+document.getElementById("Patvirtinti").addEventListener("click", function() { // paimtas elemetnas patvirtinti fiksuojamas jo paspaudimas, paspaudimo metu sukuriami kintamieji sk1 sk2
 
     let sk1, sk2;
-    sk1 = parseFloat(document.getElementById("sk1").value);
+    sk1 = parseFloat(document.getElementById("sk1").value); // priskiriamos reiksmes
     sk2 = parseFloat(document.getElementById("sk2").value);
 
 // objektas pasiima reiksmes is getElementById laukelio
-    const skaiciuotuvas = {
-        sk1: sk1,
-        sk2: sk2,
-        sudetis: function() {
-            let suma;
-            suma= this.sk1 + this.sk2;
-            return suma;
-        },
-        atimtis: function() {
-            let atimtis;
-            atimtis= this.sk1 - this.sk2;
-            return atimtis;
-        },
+// Pirmas metodas perduoti kintamaji
+//     const skaiciuotuvas = {
+//         sk1: sk1,
+//         sk2: sk2,
+//         sudetis: function() {
+//             let suma;
+//             suma= this.sk1 + this.sk2;
+//             return suma;
+//         },
+//         atimtis: function() {
+//             let atimtis;
+//             atimtis= this.sk1 - this.sk2;
+//             return atimtis;
+//         },
     
-        daugyba: function() {
-            let daugyba;
-            daugyba= this.sk1 * this.sk2;
-            return daugyba;
-        },
+//         daugyba: function() {
+//             let daugyba;
+//             daugyba= this.sk1 * this.sk2;
+//             return daugyba;
+//         },
     
-        dalyba: function() {
-            let dalyba;
-            dalyba= this.sk1 / this.sk2;
-            return dalyba;
-        },
+//         dalyba: function() {
+//             let dalyba;
+//             dalyba= this.sk1 / this.sk2;
+//             return dalyba;
+//         },
     
-        rezultatas: function() {
-            console.log(this.sudetis());
-            console.log(this.atimtis());
-            console.log(this.daugyba());
-            console.log(this.dalyba());
-            // return
-        }
+//         rezultatas: function() {
+//             console.log(skaiciuotuvas.sudetis());
+//             console.log(skaiciuotuvas.atimtis());
+//             console.log(skaiciuotuvas.daugyba());
+//             console.log(skaiciuotuvas.dalyba());
+//             // return
+//         }
+//     }
+//     skaiciuotuvas.rezultatas()
+// } )
+
+//antras metodas perduoti kintamaji 
+const skaiciuotuvas = {
+    sk1: 10,
+    sk2: 11,
+
+    paleidimas: function(a,b) {
+        this.sk1 = a;
+        this.sk2 = b;
+    },
+
+    sudetis: function() {
+        let suma;
+        suma= this.sk1 + this.sk2;
+        return suma;
+    },
+    atimtis: function() {
+        let atimtis;
+        atimtis= this.sk1 - this.sk2;
+        return atimtis;
+    },
+
+    daugyba: function() {
+        let daugyba;
+        daugyba= this.sk1 * this.sk2;
+        return daugyba;
+    },
+
+    dalyba: function() {
+        let dalyba;
+        dalyba= this.sk1 / this.sk2;
+        return dalyba;
+    },
+
+    rezultatas: function() {
+        console.log(this.sudetis());
+        console.log(this.atimtis());
+        console.log(this.daugyba());
+        console.log(this.dalyba());
+        // return
     }
-    skaiciuotuvas.rezultatas()
-} );
+};
+
+skaiciuotuvas.paleidimas(sk1,sk2);
+skaiciuotuvas.rezultatas();
+
+})
